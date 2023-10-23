0e43cc6a3544fce812e625388fcfca24313b5dd4
GreatMar2 <greatmar2@gmail.com>
2023-10-23 08:18:00 +0100

    Unit tests aside from database management module working

---
+++ EngineBay.Blueprints.Tests/Bases/BaseTestWithDbContext.cs
@@ -0,0 +1,55 @@
+﻿namespace EngineBay.Blueprints.Tests
+{
+    using EngineBay.Persistence;
+    using Microsoft.EntityFrameworkCore;
+
+    public class BaseTestWithDbContext<TContext> : IDisposable
+        where TContext : ModuleDbContext
+    {
+        private bool isDisposed;
+
+        protected BaseTestWithDbContext(string databaseName)
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<ModuleWriteDbContext>()
+                    .UseInMemoryDatabase(databaseName)
+                    .EnableSensitiveDataLogging()
+                    .Options;
+
+            var context = Activator.CreateInstance(typeof(TContext), dbContextOptions) as TContext;
+            ArgumentNullException.ThrowIfNull(context);
+
+            this.DbContext = context;
+            this.DbContext.Database.EnsureDeleted();
+            this.DbContext.Database.EnsureCreated();
+        }
+
+        protected TContext DbContext { get; set; }
+
+        /// <inheritdoc/>
+        // Dispose() calls Dispose(true)
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <inheritdoc/>
+        // The bulk of the clean-up code is implemented in Dispose(bool)
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.isDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // free managed resources
+                this.DbContext.Database.EnsureDeleted();
+                this.DbContext.Dispose();
+            }
+
+            this.isDisposed = true;
+        }
+    }
+}
---
+++ EngineBay.Blueprints.Tests/Bases/BaseTestWithFullAuditedDb.cs
@@ -0,0 +1,43 @@
+﻿namespace EngineBay.Blueprints.Tests
+{
+    using System;
+    using EngineBay.Auditing;
+    using EngineBay.Authentication;
+    using EngineBay.Persistence;
+    using Microsoft.EntityFrameworkCore;
+
+    public class BaseTestWithFullAuditedDb<TContext> : BaseTestWithDbContext<AuditingWriteDbContext>
+        where TContext : ModuleDbContext
+    {
+        public BaseTestWithFullAuditedDb(string databaseName)
+            : base(databaseName + "AuditDb")
+        {
+            this.AuditDbContext = base.DbContext;
+
+            var dbContextOptions = new DbContextOptionsBuilder<ModuleWriteDbContext>()
+                    .UseInMemoryDatabase(databaseName)
+                    .EnableSensitiveDataLogging()
+                    .Options;
+
+            var currentIdentity = new SystemUserIdentity();
+            var interceptor = new AuditingInterceptor(currentIdentity, this.AuditDbContext);
+
+            var context = Activator.CreateInstance(typeof(TContext), dbContextOptions, interceptor) as TContext;
+            ArgumentNullException.ThrowIfNull(context);
+
+            this.DbContext = context;
+            this.DbContext.Database.EnsureDeleted();
+            this.DbContext.Database.EnsureCreated();
+        }
+
+        protected new TContext DbContext { get; set; }
+
+        protected AuditingWriteDbContext AuditDbContext { get; set; }
+
+        protected void ResetAuditEntries()
+        {
+            this.AuditDbContext.AuditEntries.RemoveRange(this.AuditDbContext.AuditEntries);
+            this.AuditDbContext.SaveChanges();
+        }
+    }
+}
